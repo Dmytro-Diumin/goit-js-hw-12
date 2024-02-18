a96540004adb4056c091d2e62e027673054ff365
Dmytro-Diumin <microb201196@gmail.com>
2024-02-18 19:48:14 +0200

    create hw12

---
+++ src/js/refs.js
@@ -0,0 +1,9 @@
+export const refs = {
+    formBlock: document.querySelector('.form-block'),
+    formElem: document.querySelector('.form-elem'),
+    formInput: document.querySelector('.form-input'),
+    formBtn: document.querySelector('.form-btn'),
+    loader: document.querySelector('.loader-block'),
+    gallery: document.querySelector('.gallery'),
+    loadBtn: document.querySelector('.load-btn'),
+};
