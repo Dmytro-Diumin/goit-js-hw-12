2df8a15385c9d07be10f2974e7e1503dd37bd73e
Dmytro-Diumin <microb201196@gmail.com>
2024-02-18 19:45:31 +0200

    Revert "create hw-12"
    This reverts commit 536a91f65252d7c66b00bcee4405a58718a777ed.

--- src/js/refs.js
+++
@@ -1,9 +0,0 @@
-export const refs = {
-    formBlock: document.querySelector('.form-block'),
-    formElem: document.querySelector('.form-elem'),
-    formInput: document.querySelector('.form-input'),
-    formBtn: document.querySelector('.form-btn'),
-    loader: document.querySelector('.loader-block'),
-    gallery: document.querySelector('.gallery'),
-    loadBtn: document.querySelector('.load-btn'),
-};
--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { refs } from "./js/refs";
-import { onFormSubmit } from "./js/onFormSubmit";
-import { onLoadMoreClick } from "./js/onFormSubmit";
-
-refs.formElem.addEventListener('submit', onFormSubmit);
-refs.loadBtn.addEventListener('click', onLoadMoreClick);
